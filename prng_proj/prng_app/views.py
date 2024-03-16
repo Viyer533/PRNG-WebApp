103b328274cc5b037f1cf33fd198088f0d8235c4
ronithrnair <ronithrnair@gmail.com>
2024-03-16 13:57:21 +0530

    Fix Image Upload

--- prng_proj/prng_app/views.py
+++ prng_proj/prng_app/views.py
@@ -1,37 +1,38 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import View
 import os
 from prng_ecga import PRNS_GENERATOR
-# Create your views here.
-def home_view(request):
-    print(PRNS_GENERATOR.generate())
-    return render(request, 'index.html')
 
+class HomePage(View) :
 
-def process_form(request):
-    if request.method == 'POST' and request.FILES['imageUpload']:
-        print("Hello! Upload")
-
-        #intialize the save path for uploaded and segmented files
-        init_save_path = "Images"
-        
-        # Save the uploaded file
-        image_file = request.FILES['imageUpload']
-        print(type(image_file))
-        # image_path = init_save_path+"/assets/"+image_file.name
-        
-        # print("Uploaded Image file: ",image_file)
-        # print("Uploaded Image path: ",image_path)
-        
-        # # Perform segmentation using main.py
-        number1 = request.POST.get('number1')
-        number2 = request.POST.get('number2')
-        number3 = request.POST.get('number3')
-        number4 = request.POST.get('number4')
-        
-
-        # Process the form data (e.g., save the image, perform calculations with numbers, etc.)
-        
-        return HttpResponse("Form submitted successfully.")
-    else:
+    def get(self, request, *args, **kwargs):
         return render(request, 'index.html')
+
+
+    def post(self, request, *args, **kwargs):
+
+        print('Hello')
+        if request.method == 'POST' and request.FILES['imageUpload']:
+            print("Hello! Upload")
+
+            image_file = request.FILES['imageUpload']
+            print(type(image_file))
+            image_path = os.getcwd() + "/assets/"+image_file.name
+            
+            with open(image_path, 'wb') as destination:
+                for chunk in image_file.chunks():
+                    destination.write(chunk)
+
+            number1 = request.POST.get('number1')
+            number2 = request.POST.get('number2')
+            number3 = request.POST.get('number3')
+            number4 = request.POST.get('number4')
+
+            print(PRNS_GENERATOR.generate({'img' : image_path}))
+
+            # Process the form data (e.g., save the image, perform calculations with numbers, etc.)
+            
+            return HttpResponse("Form submitted successfully.")
+        else:
+            return render(request, 'index.html')
--- prng_proj/prng_proj/urls.py
+++ prng_proj/prng_proj/urls.py
@@ -15,11 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from prng_app import views
 
+
 urlpatterns = [
-    path('', views.home_view, name='home'),
-    # path('admin/', admin.site.urls),
-    #path('prng_app/', include('prng_app.views')),
+    path('admin/', admin.site.urls),
+    path('', include('prng_app.urls'))
 ]
